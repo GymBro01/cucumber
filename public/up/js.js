8614f9db21de3bf70c34d1a3b44fc9dc69b1e26c
GymBro01 <lukasevicevgenij47@gmail.com>
2024-11-15 18:23:50 +0300

    first

---
+++ public/up/js.js
@@ -0,0 +1,11 @@
+
+let newAkk=JSON.stringify(
+    {
+        name: document.getElementById("l").value,
+        ava: document.getElementById("ava").value,
+        email: document.getElementById("e").value,
+        password: document.getElementById("p1").value,
+        birthD: document.getElementById("b").value
+    }
+)
+
